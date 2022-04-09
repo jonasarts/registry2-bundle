7f79f11858e7b94e8d6702347ac91e3cb7d6e501
Jonas Hauser <hauser@hasch.io>
2022-04-09 21:22:45 +0200

    Fixed namespace

--- src/Form/Type/SystemType.php
+++ src/Form/Type/SystemType.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * with this source code in the file LICENSE.
  */
 
-namespace jonasarts\Bundle\RegistryBundle\Form;
+namespace jonasarts\Bundle\RegistryBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
